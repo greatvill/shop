54fc630efdc2d2cf94c63e7b2f9873a43e050073
Maxim Velikoselskih <maxv@eljur.team>
2021-10-10 13:32:31 +0700

    added fill order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'sum',
+        'client_id',
+        'manager_id',
+    ];
 }
